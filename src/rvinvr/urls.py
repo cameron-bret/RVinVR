d881aeaea67b7acd82b9c4ee78200608030e0003
cameron-bret <c.bret@whiskerhead.com>
2019-03-12 16:55:05 -0400

    refactored html using django template language, added index and blog views and urls

--- src/rvinvr/urls.py
+++ src/rvinvr/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from .views import index, blog
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog),
 ]
 
 if settings.DEBUG:
