f864f52e616de9c530ead710ca01d765524033cb
cameron-bret <c.bret@whiskerhead.com>
2019-03-13 09:48:06 -0400

    updated post views for pagination

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import SignUp
@@ -20,8 +21,20 @@ def index(request):
 
 def blog(request):
   post_list = Post.objects.all()
+  paginator = Paginator(post_list, 1)
+  page_request_var = 'page'
+  page = request.GET.get(page_request_var)
+  try:
+    paginated_queryset = paginator.page(page)
+  except PageNotAnInteger:
+    paginated_queryset = paginator.page(1)
+  except EmptyPage:
+    paginated_queryset = paginator.page(paginator.num_pages)
+
+
   context = {
-    'post_list': post_list
+    'post_list': paginated_queryset,
+    'page_request_var': page_request_var
   }
   return render(request, 'blog.html', context)
 
