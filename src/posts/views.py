993da220e77bfdbb469b5572aa7ccdd3297f4687
cameron-bret <c.bret@whiskerhead.com>
2019-03-12 22:13:32 -0400

    posts are now rendering from db but need to be styled

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-  return render(request, 'index.html', {})
+  queryset = Post.objects.filter(featured=True)
+  context = {
+    'object_list': queryset
+  }
+  return render(request, 'index.html', context)
 
 def blog(request):
   return render(request, 'blog.html', {})
