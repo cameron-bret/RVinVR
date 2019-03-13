d636bb75297819e7175a2ed811c9972d8606c855
cameron-bret <c.bret@whiskerhead.com>
2019-03-12 21:37:53 -0400

    added feature boolean value to post model, made migrations, created superuser

--- src/posts/models.py
+++ src/posts/models.py
@@ -25,6 +25,7 @@ class Post(models.Model):
   author = models.ForeignKey(Author, on_delete=models.CASCADE)
   thumbnail = models.ImageField()
   categories = models.ManyToManyField(Category)
+  featured = models.BooleanField()
 
   def __str__(self):
       return self.title
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  return render(request, 'index.html', {})
+
+def blog(request):
+  return render(request, 'blog.html', {})
+
+def post(request):
+  return render(request, 'post.html', {})
