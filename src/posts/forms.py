f7545320d6c2288059f629434fad9f4a0b1ae6e2
cameron-bret <c.bret@whiskerhead.com>
2019-03-15 03:43:22 -0400

    added tinymce

---
+++ src/posts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from tinymce import TinyMCE
+from .models import Post
+
+
+class TinyMCEWidget(TinyMCE):
+    def use_required_attribute(self, *args):
+        return False
+
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=TinyMCEWidget(
+            attrs={'required': False, 'cols': 30, 'rows': 10}
+        )
+    )
+
+    class Meta:
+        model = Post
+        fields = '__all__'
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,4 @@
+from tinymce import HTMLField
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -22,6 +23,7 @@ class Post(models.Model):
   title = models.CharField(max_length=100)
   overview = models.TextField()
   timestamp = models.DateTimeField(auto_now_add=True)
+  content = HTMLField()
   comment_count = models.IntegerField (default = 0)
   view_count = models.IntegerField (default = 0)
   author = models.ForeignKey(Author, on_delete=models.CASCADE)
