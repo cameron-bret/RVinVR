ad4ea17244ee65733a7febcd54478d4ad19850a3
cameron-bret <c.bret@whiskerhead.com>
2019-03-15 05:36:54 -0400

    comments working on posts, need styling still

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from tinymce import TinyMCE
-from .models import Post
+from .models import Post, Comment
 
 
 class TinyMCEWidget(TinyMCE):
@@ -18,3 +18,14 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Type your comment',
+        'id': 'usercomment',
+        'rows': '4',
+    }))
+    class Meta:
+        model = Comment
+        fields = ('content', )
--- src/posts/models.py
+++ src/posts/models.py
@@ -39,3 +39,17 @@ class Post(models.Model):
       return reverse('post-detail', kwargs={
         'id': self.id
       })
+  
+  @property
+  def get_comments(self):
+      return self.comments.all().order_by('-timestamp')
+
+
+class Comment(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  content = models.TextField()
+  post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.user.username
