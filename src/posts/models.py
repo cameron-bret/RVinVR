27ffb15a65ed63ed327720e085e565a7ac703abe
cameron-bret <c.bret@whiskerhead.com>
2019-03-15 04:27:28 -0400

    added previous and next foreign keys to post model and rendered on post page

--- src/posts/models.py
+++ src/posts/models.py
@@ -30,6 +30,8 @@ class Post(models.Model):
   thumbnail = models.ImageField()
   categories = models.ManyToManyField(Category)
   featured = models.BooleanField()
+  previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
+  next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
 
   def __str__(self):
       return self.title
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Count, Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from marketing.models import SignUp
 
@@ -67,4 +67,8 @@ def blog(request):
   return render(request, 'blog.html', context)
 
 def post(request, id):
-  return render(request, 'post.html', {})
+  post = get_object_or_404(Post, id=id)
+  context = {
+    'post': post
+  }
+  return render(request, 'post.html', context)
