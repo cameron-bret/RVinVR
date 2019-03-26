b9f8691a8f14b18e39f8a3b6595fc02f85b6db35
cameron-bret <c.bret@whiskerhead.com>
2019-03-26 00:31:11 -0400

    django allauth up and running

--- src/posts/views.py
+++ src/posts/views.py
@@ -94,7 +94,10 @@ def post(request, id):
   category_count = get_category_count()
   most_recent = Post.objects.order_by('-timestamp')[:3]
   post = get_object_or_404(Post, id=id)
-  PostView.objects.get_or_create(user=request.user, post=post)
+
+  if request.user.is_authenticated:
+    PostView.objects.get_or_create(user=request.user, post=post)
+    
   form = CommentForm(request.POST or None)
   if request.method == "POST":
       if form.is_valid():
